b541798a660ce0277429fad0b4c7d66e79ea46cc
DoggowithaKeyboard <gaincarlo48@gmail.com>
2025-04-15 13:27:30 -0700

    Added EE's StripMenuHiddenComponent and modified StrippableBoundUserInterface using code from frontier/Goob

--- Content.Client/Inventory/StrippableBoundUserInterface.cs
+++ Content.Client/Inventory/StrippableBoundUserInterface.cs
@@ -24,6 +24,7 @@ using Robust.Shared.Input;
 using Robust.Shared.Map;
 using static Content.Client.Inventory.ClientInventorySystem;
 using static Robust.Client.UserInterface.Control;
+using Content.Shared._EE.Strip.Components; // Harmony Change: EE
 
 namespace Content.Client.Inventory
 {
@@ -64,9 +65,11 @@ namespace Content.Client.Inventory
         {
             base.Open();
 
-            _strippingMenu = this.CreateWindowCenteredLeft<StrippingMenu>();
+            _strippingMenu = this.CreateWindow<StrippingMenu>();
             _strippingMenu.OnDirty += UpdateMenu;
             _strippingMenu.Title = Loc.GetString("strippable-bound-user-interface-stripping-menu-title", ("ownerName", Identity.Name(Owner, EntMan)));
+
+            _strippingMenu?.OpenCenteredLeft();
         }
 
         protected override void Dispose(bool disposing)
@@ -170,7 +173,9 @@ namespace Content.Client.Inventory
                     button.BlockedRect.MouseFilter = MouseFilterMode.Ignore;
             }
 
-            UpdateEntityIcon(button, hand.HeldEntity);
+            // Goobstation + Harmony : use virtual entity if hidden
+            UpdateEntityIcon(button, EntMan.HasComponent<StripMenuHiddenComponent>(hand.HeldEntity) ? _virtualHiddenEntity : hand.HeldEntity);
+            // End Goobstation + Harmony
             _strippingMenu!.HandsContainer.AddChild(button);
         }
 
@@ -211,6 +216,11 @@ namespace Content.Client.Inventory
             // this does not work for modified clients because they are still sent the real entity
             if (entity != null && _strippable.IsStripHidden(slotDef, _player.LocalEntity))
                 entity = _virtualHiddenEntity;
+
+            // Goobstation/EE + Harmony Change: hide strip menu items
+            if (entity != null && EntMan.HasComponent<StripMenuHiddenComponent>(entity))
+                entity = _virtualHiddenEntity;
+            // End Goobstation/EE + Harmony
 
             var button = new SlotButton(new SlotData(slotDef, container));
             button.Pressed += SlotPressed;
